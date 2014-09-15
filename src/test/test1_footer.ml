ac970eed5e58d27f690080bae225187f2037375c
Sebastien Mondet <seb@mondet.org>
2014-09-14 22:43:56 -0400

    test: add testing to the `sink` values

--- src/test/test1_footer.ml
+++ src/test/test1_footer.ml
@@ -2,18 +2,19 @@
 open Printf
 let say fmt = ksprintf (printf "%s\n%!") fmt
 let () =
-  let json =
-    CConv.into Tuple.source CConvYojson.sink 
-      (
-        42, 
-        `Ok ([|
-            `Two ({Simple_record. one = "oooooo"; two = Some [| "aaa"; "bbb" |];
-                   three = `Float 3.14}, 43);
-            `One_more (Some 42);
-          |]),
-        AnotherModule.(Lambda ("x", App (Lambda ("y", App (Var "y", Var "x")), Var "x")))
-      )
+  let value =
+    (
+      42, 
+      `Ok ([|
+          `Two ({Simple_record. one = "oooooo"; two = Some [| "aaa"; "bbb" |];
+                 three = `Float 3.14}, 43);
+          `One_more (Some 42);
+        |]),
+      AnotherModule.(Lambda ("x", App (Lambda ("y", App (Var "y", Var "x")), Var "x")))
+    ) 
   in
+  let json = CConv.into Tuple.source CConvYojson.sink  value in
   let s = Yojson.Basic.to_string ~std:true json in
-  say "%s" s
+  say "%s" s;
+  assert (CConv.from CConvYojson.source Tuple.sink json = value)
   
