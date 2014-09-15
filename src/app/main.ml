2792a3763ecfb290172416be62ef004d952da525
Sebastien Mondet <seb@mondet.org>
2014-09-14 23:54:32 -0400

    app: parse command line with `Arg`

--- src/app/main.ml
+++ src/app/main.ml
@@ -5,27 +5,46 @@ let say fmt = Printf.(ksprintf (eprintf "%s\n%!") fmt)
 let failwithf fmt = Printf.(ksprintf failwith fmt)
 
 let () =
-  match Array.to_list Sys.argv with
-  | _ :: [input; output] ->
-    let atd = Atd_util.load_file ~inherit_variants:false input in
-    let (full_module, original_types) = atd in
-    let (head, body) = full_module in
-    let o = open_out output in
-    List.iter body ~f:(fun item ->
-        match Atd2cconv.transform_module_item item with
-        | `Ok doc ->
-          SmartPrint.to_out_channel 78 2 o doc;
-          output_string o "\n\n"
-        | `Error (`Not_implemented msg) ->
-          close_out o;
-          failwithf "Error: %S not implemented" msg
-      );
-    close_out o;
-    ()
-  | other ->
-    say "error: wrong command line";
-    say "usage: %s <input-file> <output-file>" Sys.argv.(0);
-    ()
+  let inherit_variants = ref false in
+  let input = ref None in
+  let output = ref None in
+  let options = Arg.(align [
+      "-inline-inherit-variants",
+      Bool (fun v -> inherit_variants := v),
+      "<true|false> \n\tWrite inheriting variants inline (default: false)";
+      "-i",
+      String (fun s -> input := Some s),
+      "<file> \n\tInput ATD file (default: stdin)";
+      "-o",
+      String (fun s -> output := Some s),
+      "<file> \n\tOutput ML file (default: stdout)";
+    ]) in
+  let annons s = say "Dunno what to do with %S" s in
+  let usage = sprintf "%s [OPTIONS] <input.atd> <output.ml>" Sys.argv.(0) in
+  Arg.parse options annons usage;
+  let (i, closi), (o, closo) =
+    Option.value_map ~default:(stdin, fun _ -> ()) !input 
+      ~f:(fun f -> open_in f, close_in),
+    Option.value_map ~default:(stdout, fun _ -> ()) !output 
+      ~f:(fun f -> open_out f, close_out) in
+  let atd = 
+    Atd_util.read_channel 
+      ~inherit_fields:true
+      ~inherit_variants:!inherit_variants i in
+  let (full_module, original_types) = atd in
+  let (head, body) = full_module in
+  List.iter body ~f:(fun item ->
+      match Atd2cconv.transform_module_item item with
+      | `Ok doc ->
+        SmartPrint.to_out_channel 78 2 o doc;
+        output_string o "\n\n"
+      | `Error (`Not_implemented msg) ->
+        close_out o;
+        failwithf "Error: %S not implemented" msg
+    );
+  closi i;
+  closo o;
+  ()
 
 
 
